2810e565a8aff0c80a7d64dd15b6bf638175a340
Apple <apple@Apples-MacBook-Pro-46.local>
2019-07-09 15:28:01 -0500

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+func divisible (number : Int){
+    for index in 1...10 {
+        if number % index == 0{
+            print ("/(number) is divisible by ")
+        }
+        
+    }
+}
